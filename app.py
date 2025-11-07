cffa410852c23b9b8a080b29eab0b39c19562b7a
Wauplin <lucainp@gmail.com>
2025-11-07 16:32:06 +0100

    redirect / to /web

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from fastapi.responses import RedirectResponse
 from openenv_core.env_server import create_web_interface_app
 
 from jssp_openenv.examples import FT06
@@ -6,6 +7,14 @@ from jssp_openenv.server.jssp_environment import JSSPEnvironment
 
 env = JSSPEnvironment(FT06)
 app = create_web_interface_app(env, JSSPAction, JSSPObservation, "JSSP (FT06)")
+
+
+# Hack: redirect / to /web
+@app.get("/")
+async def redirect_to_web():
+    # TODO: should be done in openenv directly ?
+    return RedirectResponse(url="/web")
+
 
 if __name__ == "__main__":
     import uvicorn
